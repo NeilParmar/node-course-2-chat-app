1f3a3b1f2d338b0736deca0122f1acfe9863c5ee
Neil <>
2017-10-02 22:23:10 -0400

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -21,6 +21,18 @@ io.on('connection', (socket) => {
   //   createdAt: 123
   // });
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chatting application',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User Joined',
+    createdAt: new Date().getTime()
+  });
+
 //Emits an event to all the connection-
   socket.on('createMessage', (message) => {
     console.log('createMessage',message);
@@ -29,6 +41,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
